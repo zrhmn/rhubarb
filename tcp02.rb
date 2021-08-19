fd3e59e91fef7c7146fa6c489a5ac8791c23c073
Zia Ur Rehman <me@zia.im>
2021-08-18 23:05:38 -0400

    Fix example: Syntax Spice

--- tcp02.rb
+++ tcp02.rb
@@ -2,9 +2,9 @@ require 'socket'
 
 puts "Initialization time is #{Time.now()}"
 
-TCPServer.open('127.0.0.1', 8086) { |server|
-  puts "Server is now listening on port #{server.addr[2]}"
-  while conn = server.accept
+TCPServer.open(8086) do |server|
+  puts "Server is now listening on port #{server.addr[1]}"
+  while conn = server.accept()
     time = Time.now()
 
     puts "Connection accepted "\
@@ -14,6 +14,4 @@ TCPServer.open('127.0.0.1', 8086) { |server|
     conn.puts time.gmtime().strftime('%FT%Tz')
     conn.close()
   end
-
-  server.close
-}
+end
